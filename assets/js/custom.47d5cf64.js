b5a11c0107a4d3df4370222f85cd0aca2a70c263
Erik LaBianca <erik.labianca@gmail.com>
2015-07-18 13:51:39 -0400

    Built SeventhSenseMarketing from commit 69bfbdf on branch master

---
+++ assets/js/custom.47d5cf64.js
@@ -0,0 +1,37 @@
+"use strict";
+
+(function ($) {
+
+    var Sense = Sense || {};
+
+    Sense.Newsletter = Sense.Newsletter || {
+        /**
+        * Post our form
+        * Remove the newsletter form from the DOM
+        * Show thank you message
+        **/
+        submitForm: function () {
+            var $form = $('#newsletterForm'),
+                emailAddress = $form.find('input[name="cm-gtuji-gtuji"]').val(),
+                url = $form.attr("action"),
+                postForm = $.post(url, {'cm-gtuji-gtuji': emailAddress});
+
+            postForm.done(function () {
+                $('#newsletterForm').remove();
+                $('#newsletterThankYou').removeClass('dn');
+            });
+        }
+    };
+
+    $(document).ready(function () {
+
+        /* Newsletter submission handler */
+        $("#newsletterForm").submit(function (event) {
+            /* Stop standard form submission */
+            event.preventDefault();
+            Sense.Newsletter.submitForm();
+        });
+
+    });
+
+})(jQuery);
