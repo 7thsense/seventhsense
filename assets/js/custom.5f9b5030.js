373ffcde48a38596297a683cef154f695bdb94a0
smadden <scott.madden@twcable.com>
2015-07-20 17:39:13 -0400

    Built SeventhSenseMarketing from commit 3fa08df on branch master

--- assets/js/custom.5f9b5030.js
+++ assets/js/custom.5f9b5030.js
@@ -41,6 +41,33 @@
             Sense.Newsletter.submitForm();
         });
 
+        /* Start sticky header */
+        $(window).load(function () {
+            $(".sticky").sticky({
+                topSpacing: 0
+            });
+        });
+
+        /* Parallax */
+        $(window).stellar({
+            horizontalScrolling: false,
+            responsive: true
+        });
+
     });
 
 })(jQuery);
+
+
+//parallax
+$(document).ready(function () {
+    $(window).stellar({
+        horizontalScrolling: false,
+        responsive: true/*,
+         scrollProperty: 'scroll',
+         parallaxElements: false,
+         horizontalScrolling: false,
+         horizontalOffset: 0,
+         verticalOffset: 0*/
+    });
+});
